1a40ca8b607ebfe9e9a68ef216e3da34725fc386
JelenaMS <76686289+JelenaMS@users.noreply.github.com>
2021-02-09 15:41:50 +0100

    Wind,humidity and description added

--- src/index.js
+++ src/index.js
@@ -26,11 +26,19 @@ function formatDate(date) {
 function showWeather(response) {
    let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
      celsiusTemperature = response.data.main.temp;
+     
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name; 
-  
+   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function searchCity(city) {
